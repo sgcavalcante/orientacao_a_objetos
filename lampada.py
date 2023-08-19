d7e7e560da6dd3d3789f5a7d306d6a7b9e00b031
Sergio Cavalcante <a95129895@rn.senac.local>
2023-08-19 09:48:41 -0300

    orientacao a objetos

--- lampada.py
+++ lampada.py
@@ -1,21 +1,35 @@
 class Lampada():
     #atributos
-    potencia = 0
-    tensao = 220
-    cor = "branca"
-    estado = False
+    __potencia = 0  #dois underlines torna o atributo privado
+    __tensao = 220
+    __cor = "branca"
+    __estado = False
 
 
-
+    #construtor
+    def __init__(self, cor, potencia, tensao):
+        self.__cor = cor
+        self.__potencia = potencia
+        self.__tensao = tensao
 
     #métodos
     def ligar(self): #sempre temm que passar o contexto
-        self.estado = True
+        self.__estado = True
 
     def desligar(self): #sempre temm que passar o contexto
-        self.estado = False
+        self.__estado = False
     
+    def get_tensao(self):
+        return self.__tensao
     
+    def get_cor(self):
+        return self.__cor
+    
+    def get_potencia(self):
+        return self.__potencia
+    
+    def get_estado(self):
+        return self.__estado
     
     def iluminar(self):
         pass    
--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-from Lampada import lampada
-import lampada from Lampada
+from lampada import Lampada
+
+#chamada da Class Lampada()
+
+
+#sem construtor
+#lampada1 = Lampada()
+#lampada1.cor = "amarela"
+#lampada1.potencia = 20
+#lampada1.tensao = 220
+#lampada1.ligar()
+
+#com construtor
+
+lampada1 = Lampada("amarela", 500, 240)
+lampada1.ligar()
+
+
+
+print(lampada1.get_tensao())
+print(lampada1.get_cor())
+print(lampada1.get_potencia())
+print(lampada1.get_estado())
+
+print(f'Minha lampada é {lampada1.get_cor()}, de potencia {lampada1.get_cor()} para ser usada em {lampada1.get_tensao()} volts e está {lampada1.get_estado()}.')
